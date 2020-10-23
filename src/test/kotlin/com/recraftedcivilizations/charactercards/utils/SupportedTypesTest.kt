fb3943d0d2997b54bf3ce3aab87e988348642def
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-23 16:38:11 +0200

    feat: test for SupportedTypes

---
+++ src/test/kotlin/com/recraftedcivilizations/charactercards/utils/SupportedTypesTest.kt
@@ -0,0 +1,28 @@
+package com.recraftedcivilizations.charactercards.utils
+
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
+
+internal class SupportedTypesTest{
+
+    fun returnAsAny(x: Any) : Any{
+        return x
+    }
+
+    @TestFactory
+    fun castSuccessFactory() : Collection<DynamicTest>{
+        val mutableSet: MutableSet<DynamicTest> = emptySet<DynamicTest>().toMutableSet()
+
+        for(type in SupportedTypes.values()){
+            val test = DynamicTest.dynamicTest(
+                "shouldCastTo${type.name.toLowerCase().capitalize()}"
+            ) {
+                val anyType = type.cast(returnAsAny(type.exampleVal))
+                assert(anyType == type.exampleVal)
+            }
+            mutableSet.add(test)
+        }
+        return mutableSet
+    }
+}
