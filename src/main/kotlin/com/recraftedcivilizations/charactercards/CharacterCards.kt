0b08d8bca9d8c64dd952e6baeb79605309663ac5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-23 16:32:03 +0200

    feat: some default stuff for the main class

---
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/CharacterCards.kt
@@ -0,0 +1,21 @@
+package com.recraftedcivilizations.charactercards
+
+import com.github.darkvanityoflight.recraftedcore.ARecraftedPlugin
+import com.github.darkvanityoflight.recraftedcore.configparser.ARecraftedConfigParser
+import com.recraftedcivilizations.charactercards.parser.ConfigParser
+
+class CharacterCards : ARecraftedPlugin() {
+    val configParser : ARecraftedConfigParser = ConfigParser(config)
+
+    override fun onEnable() {
+        super.onEnable()
+        saveDefaultConfig()
+        instance = this
+
+        configParser.read()
+    }
+
+    companion object{
+        var instance : ARecraftedPlugin? = null
+    }
+}
