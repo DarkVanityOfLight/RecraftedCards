43b9e23409f5da8d071e9de86ae307b9c193e848
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-31 12:24:51 +0100

    chore: add javadocs

--- src/main/kotlin/com/recraftedcivilizations/charactercards/cards/Card.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/cards/Card.kt
@@ -22,6 +22,12 @@ abstract class Card : IAmACard{
         const val FIELD_DOES_NOT_EXIST = "A field parsed is not declared in the config"
         const val FIELD_TYPE_OTHER_THEN_VALUE_TYPE = "A parsed value differs from the expected field type and could not be converted"
 
+
+        /**
+         * @param fieldMap A map that shows the Type for a field in the valueMap
+         * @param valueMap Stores the fields from the character card
+         * @return         A Pair of a boolean that indicates if the map is valid and an error message if the map is not
+         */
         fun validateMaps(fieldMap: Map<String, SupportedTypes>, valueMap: Map<String, Any>) : Pair<Boolean, String?>{
             for(key in valueMap.keys){
                 val type: SupportedTypes = fieldMap[key] ?: return Pair(false, FIELD_DOES_NOT_EXIST)
