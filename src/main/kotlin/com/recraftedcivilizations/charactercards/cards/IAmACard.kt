9e34179ef299bf144fe28dd4639d7dd31257f189
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-31 12:37:45 +0100

    feat: Add Java doc

--- src/main/kotlin/com/recraftedcivilizations/charactercards/cards/IAmACard.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/cards/IAmACard.kt
@@ -1,5 +1,10 @@
 package com.recraftedcivilizations.charactercards.cards
 
+/**
+ * @author DarkVanityOfLight
+ */
+
+
 import com.recraftedcivilizations.charactercards.utils.SupportedTypes
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
@@ -10,10 +15,35 @@ interface IAmACard : Serializable {
     var valueMap: Map<String, Any>
 
 
+    /**
+     * @return Returns a map with all fields and their Type
+     */
     fun getFields() : Map<String, SupportedTypes>
+
+    /**
+     * @param key The key for the field you want to have
+     * @return    Returns the field value or null if the field does not exist
+     */
     fun getFieldValue(key : String) : Any?
+
+    /**
+     * @param key The key for the field you want to have
+     * @return    Returns the type of the field or null
+     */
     fun getFieldType(key : String): SupportedTypes?
+
+    /**
+     * @return Returns the output representation for the Card as String
+     */
     fun getChatOutputRepresentation() : String
+
+    /**
+     * @return Returns the output representation for the Card as an Inventory GUI
+     */
     fun getGUIOutputRepresentation() : Inventory
+
+    /**
+     * @param player The player the Card should be displayed to
+     */
     fun display(player: Player)
 }
