fb1a23d8d0fb171d7019cf3ba8b23360d561e41a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-01 16:13:03 +0100

    feat: Test value serialization

--- src/test/kotlin/com/recraftedcivilizations/charactercards/utils/serializer/ValueMapSerializerTest.kt
+++ src/test/kotlin/com/recraftedcivilizations/charactercards/utils/serializer/ValueMapSerializerTest.kt
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.Assertions.*
 internal class ValueMapSerializerTest {
 
     @Test
-    fun serialize() {
-        val map = mapOf(Pair("Foo", "Bar"), Pair("Bar", 3))
+    fun shouldSerialize() {
+        val map = mapOf(Pair("Foo", "Bar"), Pair("Bar", 3), Pair("FooBar", null))
 
         val res = ValueMapSerializer().serialize(map)
         println(res)
-        assert(res == "{ \"Foo\" : \"Bar\", \"Bar\" : 3 }")
+        assert(res == "{ \"Foo\" : \"Bar\", \"Bar\" : 3, \"FooBar\" : null }")
 
     }
 }
