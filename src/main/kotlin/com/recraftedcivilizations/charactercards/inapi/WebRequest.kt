c07736fbadc576ac6425f5a48f5a2f7d0d2b10cc
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-02 18:55:49 +0100

    feat: Abstract class that implements IRequest to represent a webrequest

---
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/inapi/WebRequest.kt
@@ -0,0 +1,22 @@
+package com.recraftedcivilizations.charactercards.inapi
+
+/**
+ * Represents an Web request and serialize the request to json string
+ * @author DarkVanityOfLight
+ */
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+abstract class WebRequest : IRequest {
+
+    /**
+     * Serialize the object as an Json String
+     * @return Returns the object as an Json String
+     */
+    override fun serialize(): String {
+        return Json.encodeToString(this)
+    }
+}
