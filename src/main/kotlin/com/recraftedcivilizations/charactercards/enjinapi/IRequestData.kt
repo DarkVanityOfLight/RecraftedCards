f1b58d623da0812543cbafc911e39759b0c2e8fe
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-02 13:12:47 +0100

    feat: Interface for classes that request data

---
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/enjinapi/IRequestData.kt
@@ -0,0 +1,18 @@
+package com.recraftedcivilizations.charactercards.enjinapi
+
+
+/**
+ * Represents an object that can request data
+ * @author DarkVanityOfLight
+ */
+
+
+interface IRequestData {
+
+    /**
+     * Send an request to the defined url
+     * @param request The Request object to be sent
+     * @return An Response object containing the info
+     */
+    suspend fun request(request: Request): Response
+}
