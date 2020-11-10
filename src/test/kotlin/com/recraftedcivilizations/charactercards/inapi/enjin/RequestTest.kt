d0262785658c05cd481a2191a65212bca994ceb2
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-10 16:07:32 +0100

    feat: Test all fields of Request

--- src/test/kotlin/com/recraftedcivilizations/charactercards/inapi/enjin/RequestTest.kt
+++ src/test/kotlin/com/recraftedcivilizations/charactercards/inapi/enjin/RequestTest.kt
@@ -16,4 +16,41 @@ internal class RequestTest {
             toSerialze.serialize()
         )
     }
+
+    @Test
+    fun getId() {
+        val toTest: Request = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), "foo")
+        assertEquals("foo", toTest.id)
+    }
+
+    @Test
+    fun getMethode() {
+        val toTest: Request = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), "foo")
+        assertEquals("bar", toTest.methode)
+    }
+
+    @Test
+    fun getParams() {
+        val toTest: Request = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), "foo")
+        assertEquals(mapOf(Pair("Foo", "Bar")), toTest.params)
+    }
+
+    @Test
+    fun getJsonrpc() {
+        var toTest: Request = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), "foo")
+        assertEquals("foo", toTest.jsonrpc)
+        toTest = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), null)
+        assertEquals(null, toTest.jsonrpc)
+    }
+
+    @Test
+    fun setJsonrpc() {
+        val toTest: Request = Request("foo", "bar", mapOf(Pair("Foo", "Bar")), "foo")
+        toTest.jsonrpc = "bar"
+        assertEquals("bar", toTest.jsonrpc)
+    }
+
+    @Test
+    fun shouldGenerateHeaderAndBody() {}
+
 }
