0f3884b72a957308dde48da0729797d2bdf62b96
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-22 17:19:25 +0100

    feat: Initialize the data parser

--- src/main/kotlin/com/recraftedcivilizations/charactercards/CharacterCards.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/CharacterCards.kt
@@ -3,9 +3,11 @@ package com.recraftedcivilizations.charactercards
 import com.github.darkvanityoflight.recraftedcore.ARecraftedPlugin
 import com.github.darkvanityoflight.recraftedcore.configparser.ARecraftedConfigParser
 import com.recraftedcivilizations.charactercards.parser.ConfigParser
+import com.recraftedcivilizations.charactercards.parser.DataParser
 
 class CharacterCards : ARecraftedPlugin() {
     val configParser : ConfigParser = ConfigParser(config)
+    val dataParser : DataParser = DataParser()
 
     override fun onEnable() {
         super.onEnable()
