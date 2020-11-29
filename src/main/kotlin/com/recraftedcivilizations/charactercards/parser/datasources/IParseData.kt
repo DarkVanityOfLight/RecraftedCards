78bc54e2ae342bb1615ab420d30ed66699c904ef
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-29 12:07:11 +0100

    feat: create an interface for datasources

---
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/parser/datasources/IParseData.kt
@@ -0,0 +1,11 @@
+package com.recraftedcivilizations.charactercards.parser.datasources
+
+import com.recraftedcivilizations.charactercards.cards.Card
+import com.recraftedcivilizations.charactercards.cards.CharacterCard
+import org.bukkit.entity.Player
+
+interface IParseData {
+
+    fun getCard(player: Player): CharacterCard
+    fun setCard(player: Player, card: Card)
+}
