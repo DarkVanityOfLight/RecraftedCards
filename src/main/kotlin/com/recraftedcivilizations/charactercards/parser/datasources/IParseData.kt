3630fb65f22a87c5090ae2fe1139efb6cb92124f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-29 12:14:34 +0100

    chore: javadoc

--- src/main/kotlin/com/recraftedcivilizations/charactercards/parser/datasources/IParseData.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/parser/datasources/IParseData.kt
@@ -4,8 +4,23 @@ import com.recraftedcivilizations.charactercards.cards.Card
 import com.recraftedcivilizations.charactercards.cards.CharacterCard
 import org.bukkit.entity.Player
 
+/**
+ * A data source which can read and write cards
+ * @author DarkVanityOfLight
+ */
 interface IParseData {
 
+    /**
+     * Get a card from the data source
+     * @param player The Player which owns the card
+     * @return returns a character card or null if none is found
+     */
     fun getCard(player: Player): CharacterCard?
+
+    /**
+     * Set a card for a player
+     * @param player The owner of the card
+     * @param card The card to set
+     */
     fun setCard(player: Player, card: Card)
 }
