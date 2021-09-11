69a25652962f39af03f9604f0a1bb58ff28d26d1
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-09-11 15:05:08 +0200

    feat: Display perm nodes that start with charcards.title in the character card

--- src/main/kotlin/com/recraftedcivilizations/charactercards/cards/CharacterCard.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/cards/CharacterCard.kt
@@ -6,6 +6,7 @@ package com.recraftedcivilizations.charactercards.cards
 
 
 import com.recraftedcivilizations.charactercards.CharacterCards
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
@@ -36,7 +37,24 @@ class CharacterCard : Card {
      * @return Returns the output representation for the Card as String
      */
     override fun getChatOutputRepresentation(): String {
+
+        // Check for permission nodes that should be mentioned
+        val perms = Bukkit.getServer().getPlayer(owner)?.effectivePermissions
+        val permsToDisplay = emptyList<String>().toMutableList()
+        if (perms != null){
+            for (permission in perms){
+                if (permission.permission.startsWith("charcards.title.")){
+                    val stripedPerm = permission.permission.removePrefix("charcards.title.").replace("_"," ")
+                    permsToDisplay.add(stripedPerm)
+                }
+            }
+
+        }
+
+
         var resultString = ChatColor.translateAlternateColorCodes('&',"&b========Character Card========\n")
+        resultString += ChatColor.GREEN.toString() + "Title: ${permsToDisplay.joinToString(" ")}"
+
         for(key in fields){
             resultString += if(valueMap[key] == null){
                 ChatColor.GREEN.toString()  + "$key: Unknown\n"
