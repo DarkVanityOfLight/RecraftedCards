e6ca9092ce03b71abf568ab7540b6855b285c67e
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-09-11 14:37:51 +0200

    feat: Adapat to the use of UUIDS as owners

--- src/main/kotlin/com/recraftedcivilizations/charactercards/parser/datasources/YMLDataSource.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/parser/datasources/YMLDataSource.kt
@@ -2,15 +2,15 @@ package com.recraftedcivilizations.charactercards.parser.datasources
 
 import com.recraftedcivilizations.charactercards.cards.Card
 import com.recraftedcivilizations.charactercards.cards.CharacterCard
+import org.bukkit.OfflinePlayer
 import org.bukkit.configuration.file.YamlConfiguration
-import org.bukkit.entity.Player
 import java.io.File
 
 
 class YMLDataSource(override val dataURI: String) : IParseData {
    private val dataFile: YamlConfiguration = YamlConfiguration.loadConfiguration(File(dataURI))
 
-    override fun getCard(player: Player, fields: List<String>): CharacterCard {
+    override fun getCard(player: OfflinePlayer, fields: List<String>): CharacterCard {
         val valueMap = mapOf<String, String?>().toMutableMap()
         val cardSection = dataFile.getConfigurationSection("cards.${player.uniqueId}") ?: return CharacterCard(fields, player)
 
@@ -22,7 +22,7 @@ class YMLDataSource(override val dataURI: String) : IParseData {
     }
 
     override fun setCard(card: Card) {
-        val cardSection = dataFile.createSection("cards.${card.owner.uniqueId}", card.valueMap)
+        val cardSection = dataFile.createSection("cards.${card.owner}", card.valueMap)
         dataFile.save(File(dataURI))
 
     }
