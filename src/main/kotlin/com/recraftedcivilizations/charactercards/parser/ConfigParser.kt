fd04cf6c69b739bc4fb462e0218675571537ffed
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-09-11 14:20:20 +0200

    feat: Get a simple string list as fields from the config

--- src/main/kotlin/com/recraftedcivilizations/charactercards/parser/ConfigParser.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/parser/ConfigParser.kt
@@ -1,61 +1,28 @@
 package com.recraftedcivilizations.charactercards.parser
 
 import com.github.darkvanityoflight.recraftedcore.configparser.ARecraftedConfigParser
-import com.recraftedcivilizations.charactercards.CharacterCards
-import com.recraftedcivilizations.charactercards.utils.SupportedTypes
-import org.bukkit.Bukkit
-import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.configuration.file.FileConfiguration
 
 
 operator fun Regex.contains(text: CharSequence): Boolean = this.matches(text)
 
 class ConfigParser(config: FileConfiguration) : ARecraftedConfigParser(config) {
-    var fields: Map<String, SupportedTypes>? = null
+    var fields: List<String> = emptyList()
     var defaultMode: String = "TEXT"
     var dataSource: String = "YML"
 
     override fun read() {
-        val fieldsSection = config.getConfigurationSection("fields")
-        if (fieldsSection == null) {
-            CharacterCards.instance!!.severe("You didn't specify any fields for your character cards")
-            Bukkit.getPluginManager().disablePlugin(CharacterCards.instance!!)
-            return
-        }
-        fields = parseFields(fieldsSection)
-
+        fields = config.getStringList("fields")
 
         val dMode = config.getString("defaultMode")
-        if(dMode != null && (dMode.toUpperCase() == "GUI" || dMode.toUpperCase() == "TEXT")){
+        if (dMode != null && (dMode.toUpperCase() == "GUI" || dMode.toUpperCase() == "TEXT")) {
             defaultMode = dMode
         }
+
         val dSource = config.getString("dataMode")
-        if (dSource != null && (dSource.toUpperCase() == "YML" || dSource.toUpperCase() == "SQL")){
+        if (dSource != null && (dSource.toUpperCase() == "YML" || dSource.toUpperCase() == "SQL")) {
             dataSource = dSource.toUpperCase()
         }
 
-    }
-
-
-    private fun parseFields(configSection: ConfigurationSection): Map<String, SupportedTypes> {
-        val parsedFields: MutableMap<String, SupportedTypes> = emptyMap<String, SupportedTypes>().toMutableMap()
-
-        for (key in configSection.getKeys(false)) {
-            if (configSection.getString(key) != null) {
-                val type = configSection.getString(key)
-                try {
-                    if (type != null) parsedFields[key] = SupportedTypes.valueOf(type.toUpperCase())
-                    else CharacterCards.instance?.warning("Could not parse type of field $key")
-                } catch (e: IllegalArgumentException){
-                    CharacterCards.instance?.warning("Type $key is not supported")
-                }
-
-
-            } else {
-                continue
-            }
-        }
-
-        return parsedFields
     }
 }
