a6a4161ac47af64dde33ea049e545cd23dc492b9
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-09-11 12:27:07 +0200

    feat: Remove field typing in the Card class

--- src/main/kotlin/com/recraftedcivilizations/charactercards/cards/Card.kt
+++ src/main/kotlin/com/recraftedcivilizations/charactercards/cards/Card.kt
@@ -11,52 +11,30 @@ import com.recraftedcivilizations.charactercards.utils.SupportedTypes
  * @property valueMap A map with all field names and their value
  */
 abstract class Card : IAmACard{
-    final override var fieldMap: Map<String, SupportedTypes>
-    final override var valueMap: MutableMap<String, Any?>
+    final override var fields: List<String>
+    final override var valueMap: MutableMap<String, String?>
 
     /**
-     * @param fieldMap A map with all fields the card should have
+     * @param fields A list containing all fields the card should have
      * @param valueMap A map with all values to initialize the card
      */
-    constructor(fieldMap: Map<String, SupportedTypes>, valueMap: Map<String, Any?>){
-        val res = validateMaps(fieldMap, valueMap)
-        if(!res.first){
-            throw IllegalArgumentException(res.second)
-        }else{
-            this.fieldMap = fieldMap
+    constructor(fields: List<String>, valueMap: Map<String, String?>){
+            this.fields = fields
             this.valueMap = valueMap.toMutableMap()
-        }
     }
 
     /**
-     * @param fieldMap A map with all fields the card should have
+     * @param fields A list with all fields the card should have
      */
-    constructor(fieldMap: Map<String, SupportedTypes>){
+    constructor(fields: List<String>){
 
-        val valueMap = emptyMap<String, Any?>().toMutableMap()
-        for(key in fieldMap.keys){
+        val valueMap = emptyMap<String, String?>().toMutableMap()
+        for(key in fields){
             valueMap[key] =  null
         }
 
-        val res = validateMaps(fieldMap, valueMap)
-        if(!res.first){
-            throw IllegalArgumentException(res.second)
-        }else{
-            this.fieldMap = fieldMap
-            this.valueMap = valueMap
-        }
-
-
-    }
-
-    /**
-     * Get the type for a given field
-     * @param key The field name to get the type from
-     * @return Returns the Type or null if the field does not exist
-     * @see SupportedTypes
-     */
-    override fun getFieldType(key: String): SupportedTypes?{
-        return fieldMap[key]
+        this.valueMap = valueMap
+        this.fields = fields
     }
 
     /**
@@ -64,64 +42,7 @@ abstract class Card : IAmACard{
      * @param key The field name
      * @return Return the value or null if field does not exist
      */
-    override fun getFieldValue(key: String): Any? {
+    override fun getFieldValue(key: String): String? {
         return valueMap[key]
-    }
-
-    /**
-     * Get the field map
-     * @return Returns a map of <String, SupportedTypes> representing all fields#
-     * @see SupportedTypes
-     */
-    override fun getFields(): Map<String, SupportedTypes> {
-        return fieldMap
-    }
-
-    /**
-     * Set a value in the value map
-     * @param key The field name to set the value
-     * @param value The value to set
-     */
-    override fun setFieldValue(key: String, value: Any) {
-        if(key in fieldMap.keys){
-            if(fieldMap[key]!!.cast(value) != null || fieldMap[key]!!.convert(value) != null){
-                valueMap[key] = value
-            }
-        }
-    }
-
-    companion object{
-        const val FIELD_DOES_NOT_EXIST = "A field parsed is not declared in the config"
-        const val FIELD_TYPE_OTHER_THEN_VALUE_TYPE = "A parsed value differs from the expected field type and could not be converted"
-
-
-        /**
-         * @param fieldMap A map that shows the Type for a field in the valueMap
-         * @param valueMap Stores the fields from the character card
-         * @return         A Pair of a boolean that indicates if the map is valid and an error message if the map is not
-         */
-        fun validateMaps(fieldMap: Map<String, SupportedTypes>, valueMap: Map<String, Any?>) : Pair<Boolean, String?>{
-            for(key in valueMap.keys){
-                val type: SupportedTypes = fieldMap[key] ?: return Pair(false, FIELD_DOES_NOT_EXIST)
-                val value = valueMap[key]
-
-                if (value != null){
-                    try{
-                        val currentType = SupportedTypes.valueOf(value::class.simpleName!!.toUpperCase())
-                        if (currentType != type){
-                            if(type.convert(value) != null){
-                                continue
-                            }else{
-                                return Pair(false, FIELD_TYPE_OTHER_THEN_VALUE_TYPE)
-                            }
-                        }
-                    }catch (e: IllegalArgumentException){
-                        return Pair(false, FIELD_TYPE_OTHER_THEN_VALUE_TYPE)
-                    }
-                }else continue
-            }
-
-            return Pair(true, null)
-        }
     }
 }
